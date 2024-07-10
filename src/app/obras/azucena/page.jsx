941269b0e76e2282d0a0764c716f3a9de43866c6
Gonzalo Perez Rojas <0xg@Gonzalos-MacBook-Pro.local>
2024-07-10 00:17:30 -0300

    agrega modelos restantes

--- src/app/obras/azucena/page.jsx
+++ src/app/obras/azucena/page.jsx
@@ -8,8 +8,20 @@ export default function Page() {
   return (
     <div className="flex min-h-screen min-w-screen flex-col items-center justify-center">
       <ModelComponent 
-        model={<Model />}  
+        model={<Model modelUrl="/models/azucenas.glb" />} 
+        target={"/targets/azucenas.mind"}
+        scale={0.005}
+        position={[0,0,0]}
+        ligths={<SceneLights />}  
         />
     </div>
   );
 }
+function SceneLights() {
+
+  return(
+  <>
+    <directionalLight position={[0,30,0]} intensity={1.5}/> 
+    <directionalLight position={[0,-30,0]} intensity={1.5}/> 
+  </>
+)}
