817c47599ec5adb91c06b85ad349a2c9a2a3e9b4
Numa-diazdiva <manuel.diazd@hotmail.com>
2024-07-11 23:30:34 -0300

    Rotación model

--- src/components/model.jsx
+++ src/components/model.jsx
@@ -1,10 +1,20 @@
+import { useRef } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { useLoader } from "@react-three/fiber";
+import { useLoader, useFrame } from "@react-three/fiber";
+import { damp } from 'three/src/math/MathUtils';
 
 export default function Model({
   modelUrl=""
 }) {
   const gltf = useLoader(GLTFLoader, modelUrl);
+  const modelRef = useRef();
+
+  useFrame(
+    (state, delta) => {
+      const modelRotationY = modelRef.current.rotation.y;
+      modelRef.current.rotation.y = damp(modelRotationY, modelRotationY + 1, 1, delta);
+    }
+  );
 
   return (
     <group>
@@ -13,6 +23,7 @@ export default function Model({
         position={[0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
         object={gltf.scene}
+        ref={modelRef}
       />
     </group>
   );
